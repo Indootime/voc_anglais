e10248c9655ebd654dc56aa667401cb4696a4ea4
Indootime <indootime@yahoo.fr>
2017-01-28 09:54:16 +0100

    Ajout de menu, Vocabulaire,score

--- Anglais.py
+++ Anglais.py
@@ -42,7 +42,7 @@ VERBS_HAVE = tuple({"j'ai": "I have",
                     "Elles sont": "They have"}.items())
 
 NATURE = tuple({"ajouter": "add",
-                "répondre" : "answer",
+                "répondre à une solution" : "answer",
                 "zone, région" : "area",
                 "croire" : "believe",
                 "oiseau" : "bird",
@@ -50,7 +50,79 @@ NATURE = tuple({"ajouter": "add",
                 "réserver" : "book up",
                 "buisson" : "bush",
                 "charmant" : "charming",
-                "poussin" : "chick"}.items())
+                "poussin" : "chick",
+                "nettoyer" : "clean up",
+                "escalader" : "climb",
+                "colonie" : "colony",
+                "protection" : "conservation",
+                "coûter" : "cost",
+                "campagne" : "countryside",
+                "crise" : "crisis",
+                "couper, abattre" : "cut down",
+                "décimer" :"decimate",
+                "mettre en danger" : "endanger",
+                "défenseur de l'environnement" : "environmentalist",
+                "estimer" : "estimate",
+                "exclamer" : "exclaim",
+                "expert" : "expert",
+                "expliquer" : "explain",
+                "fleur": "flower",
+                "forêt" : "forest",
+                "herbe" : "grass",
+                "faire pousser" : "grow",
+                "colline" : "hill",
+                "frapper" : "hit",
+                "espèrer" : "hope",
+                "îles" : "island",
+                "problème, question" : "issue",
+                "tout juste" : "just",
+                "lac" : "lake",
+                "paysage" : "landscape",
+                "merveilleux" : "marvellous",
+                "problème" :"matter",
+                "pré" : "meadow",
+                "rencontre" : "meeting",
+                "montagne" : "montain",
+                "chêne" : "oak",
+                "arbre" : "tree",
+                "bénévole" : "volunteer",
+                "bois" :"wood",
+                "cascade" : "waterfall",
+                "cueillir" : "pick up",
+                "couler" :"sink",
+                "étang" : "pond",
+                "pétrole" : "oil",
+                "verger" : "orchard",
+                "propriétaire" : "owner",
+                "marée noire" : "oil spill",
+                "pingouin" : "penguin",
+                "persuader" : "persuade",
+                "pessimiste" : "pessimistic",
+                "pin" : "pin tree",
+                "planter" : "plant",
+                "protester" : "protest",
+                "pluie" :"plain",
+                "rénover" : "renovate",
+                "répondre à quelqu'un" : "reply",
+                "rapport" : "report",
+                "repos" :"rest",
+                "rivière" : "river",
+                "abîmé" : "ruined",
+                "sauver" : "save",
+                "dire" : "say",
+                "côte, rivage" : "shore",
+                "neige" : "snow",
+                "sol" : "soil",
+                "résoudre" : "solve",
+                "sommet" : "summit",
+                "se tenir" : "take place",
+                "pétrolier" : "tanker",
+                "dire" : "tell",
+                "penser" : "think",
+                "vérité" : "truth",
+                "visiter" : "visit",
+                "promenade" : "walk",
+                "faune" :"wildlife"}.items())
 
 
 def main_menu():
@@ -68,6 +140,8 @@ def main_menu():
     print()
     print("6.  Conjugaison du Verbe avoir (Français --> Anglais)")
     print()
+    print("7.  Vocabulaire sur la nature")
+
     return input("Quel est ton choix :")
 
 
@@ -78,13 +152,14 @@ def ask(word_to_translate, translated_word):
     
     if answer.lower().strip() == translated_word.lower():
         print("Bonne réponse !")
+        return 1
     else:
         print("Non, c'est {}".format(translated_word))
+        return 0
 
 
-
-def main(choice, runs=16):
-
+def main(choice, runs=25):
+    a = 0
     for _ in range(runs):
         
         if int(choice) == 1:
@@ -99,10 +174,18 @@ def main(choice, runs=16):
             word_to_translate, translated_word = random.choice(VERBS_HAVE)
         if int(choice) == 6:
             translated_word, word_to_translate = random.choice(VERBS_HAVE)
-    
-        ask(word_to_translate, translated_word)
+        if int(choice) == 7:
+            word_to_translate,translated_word = random.choice(NATURE)
+        a=a+ask(word_to_translate, translated_word)
+        print("Score: " + str(a) + "/"+str(runs) )
 
-       
+    print()   
+    answer=input("Voulez vous faire une autre série? (o/n)")
+    print()
+
+    if answer == "o":
+        
+        main(main_menu())
         
         
             
